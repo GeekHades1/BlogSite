23dc38bb33745b25fa210f447665bae4c7424c21
GeekHades <geekhades1@gmail.com>
2018-08-03 16:49:51 +0800

    add 404.htnl

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ import markdown
 from markdown.extensions.toc import TocExtension
 
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, render_to_response
 from django.views.generic import ListView, DetailView, TemplateView
 from django.utils.text import slugify
 
@@ -300,6 +300,11 @@ class TagView(ListView):
         tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
         return super(TagView, self).get_queryset().filter(tags=tag)
 
+
+def page_not_found(request):
+    response = render_to_response('blog/404.html', {})
+    response.status_code = 404
+    return response
 
 def about(request):
     """关于我"""
